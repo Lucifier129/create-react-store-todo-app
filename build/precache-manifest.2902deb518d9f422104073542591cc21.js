1aca294af1e48acb4e58380161b2a250d56330e5
yj_gu <yj_gu@ctrip.com>
2019-11-28 16:56:43 +0800

    build

---
+++ build/precache-manifest.2902deb518d9f422104073542591cc21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0569cd0327849742f8d242e6cbfbd567",
+    "url": "/create-react-store-todo-app/build/index.html"
+  },
+  {
+    "revision": "20dd9ab27d63db341032",
+    "url": "/create-react-store-todo-app/build/static/css/main.2a4bb4e9.chunk.css"
+  },
+  {
+    "revision": "c5732064a554c96dfe23",
+    "url": "/create-react-store-todo-app/build/static/js/2.2b6778ad.chunk.js"
+  },
+  {
+    "revision": "20dd9ab27d63db341032",
+    "url": "/create-react-store-todo-app/build/static/js/main.f18bae09.chunk.js"
+  },
+  {
+    "revision": "e0ba93829ef8939b0b3e",
+    "url": "/create-react-store-todo-app/build/static/js/runtime-main.96a3c8d8.js"
+  }
+]);
